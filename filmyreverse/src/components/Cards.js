c5224016818a0b93d1c535d2bc27aad97ee470da
vithaln <vnnivargi1998@gmail.com>
2023-02-10 13:45:12 +0530

    clean code

--- filmyreverse/src/components/Cards.js
+++ filmyreverse/src/components/Cards.js
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from "react";
-import ReactStars from 'react-stars'
-import {TailSpin, ThreeDots} from 'react-loader-spinner'
+import ReactStars from "react-stars";
+import { TailSpin, ThreeDots } from "react-loader-spinner";
 import { addDoc, getDoc, getDocs } from "firebase/firestore";
 import { moviesref } from "./firebase/firebase";
 import { Link } from "react-router-dom";
 
 const Cards = () => {
-
-
   const [data, setData] = useState([
     // {
     //   name: "Pathan",
@@ -73,58 +71,60 @@ const Cards = () => {
     // },
   ]);
 
-  const[loading,setLoading]=useState(false);
+  const [loading, setLoading] = useState(false);
 
-useEffect(()=>{
+  useEffect(() => {
+    async function getData() {
+      setLoading(true);
 
-  async function getData(){
-setLoading(true);
+      const _data = await getDocs(moviesref);
 
-const _data=await getDocs(moviesref);
+      _data.forEach((doc) => {
+        setData((prev) => [...prev, { ...doc.data(), id: doc.id }]);
+      });
 
-_data.forEach((doc)=>{
-
-  setData((prev)=>[...prev,{...(doc.data()),id:doc.id}])
-})
-
-setLoading(false);
-  }
-getData();
-},[])
-
+      setLoading(false);
+    }
+    getData();
+  }, []);
 
   return (
     <div className="flex flex-wrap justify-between px-3 mt-3">
-      { loading? <div className="w-full flex  justify-center items-center h-96"> <ThreeDots/> </div>:
-      data.map((e, i) => {
-
-        return (
-<Link to={`/detail/${e.id}`}>
-          <div
-            key={i}
-            className="card cursor-pointer hover:-translate-y-2 p-3  mt-6 transition-all duration-500 "
-          >
-            <img className="h-60 md:h-72 card shadow-lg p-2 " src={e.img} />
-            <h1>
-              {/* <span className="text-gray-500">Name: </span>  */} {e.title}
-            </h1>
-            <h1>
-              <span className="text-gray-500">Year: </span> {e.year}
-            </h1>
-            <h1 className="flex items-center">
-              <span className="text-gray-500 mr-1">Rating: </span> 
-              <ReactStars 
-              size={20}
-              half={true}
-              edit={false}
-              value={e.rating/e.rated }
-              />
-            </h1>
-          </div>
-          </Link>
-        )
-      })}
-
+      {loading ? (
+        <div className="w-full flex  justify-center items-center h-96">
+          {" "}
+          <ThreeDots />{" "}
+        </div>
+      ) : (
+        data.map((e, i) => {
+          return (
+            <Link to={`/detail/${e.id}`}>
+              <div
+                key={i}
+                className="card cursor-pointer hover:-translate-y-2 p-3  mt-6 transition-all duration-500 "
+              >
+                <img className="h-60 md:h-72 card shadow-lg p-2 " src={e.img} />
+                <h1>
+                  {/* <span className="text-gray-500">Name: </span>  */}{" "}
+                  {e.title}
+                </h1>
+                <h1>
+                  <span className="text-gray-500">Year: </span> {e.year}
+                </h1>
+                <h1 className="flex items-center">
+                  <span className="text-gray-500 mr-1">Rating: </span>
+                  <ReactStars
+                    size={20}
+                    half={true}
+                    edit={false}
+                    value={e.rating / e.rated}
+                  />
+                </h1>
+              </div>
+            </Link>
+          );
+        })
+      )}
     </div>
   );
 };
--- filmyreverse/src/components/firebase/firebase.js
+++ filmyreverse/src/components/firebase/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getFirestore,collection} from 'firebase/firestore'
+import { getFirestore, collection } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,18 +14,15 @@ const firebaseConfig = {
   storageBucket: "filmeverse-e0847.appspot.com",
   messagingSenderId: "918537852029",
   appId: "1:918537852029:web:40ef942b1b618a0efacd14",
-  measurementId: "G-RB8TWF5WK7"
+  measurementId: "G-RB8TWF5WK7",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-export const db= getFirestore(app);
-export const moviesref=collection(db,"movies");
-export const reviewsref=collection(db,"reviews");
-export const usersRef=collection(db,"users");
-
-
-
+export const db = getFirestore(app);
+export const moviesref = collection(db, "movies");
+export const reviewsref = collection(db, "reviews");
+export const usersRef = collection(db, "users");
 
 export default app;
